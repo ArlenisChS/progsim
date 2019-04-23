f8762819ca0aaeefb43dddde81108d60a46584e5
Alexander Gonzalez <alexfertel97@gmail.com>
2019-04-23 16:54:39 -0400

    Add run.pl, add imports

---
+++ bfs.pl
@@ -0,0 +1,8 @@
+% Tuple structure: (dirty, obstacle, yard, child, robot)
+
+
+% Env       => The map
+% Start     => Starting position of the bfs
+% Obstacles => A list of characters that can't be visited
+% 
+% bfs(Env, Start, Obstacles)
---
+++ run.pl
@@ -0,0 +1,14 @@
+:- consult('generator.pl').
+:- consult('bfs.pl').
+:- consult('agents.pl').
+
+% :- set_prolog_flag(verbose, silent).
+:- initialization(main).
+
+main :-
+    format('Example script~n'),
+    current_prolog_flag(argv, Argv),
+    format('Called with ~q~n', [Argv]),
+    halt.
+main :-
+    halt(1).
