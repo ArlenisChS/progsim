bf406ee3d4866e3af39c3e2a8bb98bf47dd56a6a
Arlenis <arlenisching@gmail.com>
2019-04-24 03:29:11 -0400

    Fix but not tested

--- agents.pl
+++ agents.pl
@@ -10,13 +10,16 @@ col(Matrix, N, Col) :-
 index(Matrix, I, J, Tuple) :-
     row(I, Matrix, Row), nth1(J, Row, Tuple).
 
-rows(Matrix, Length) :- length(Matrix, Length).
+rows(Matrix, Length) :- 
+    length(Matrix, Length).
 
-columns(Matrix, Length) :- row(1, Matrix, Row), length(Row, Length).
+columns(Matrix, Length) :- 
+    row(1, Matrix, Row), length(Row, Length).
 
 replace([], _, _, []).
 replace([_ | List], 1, Elem, [Elem | List]).
-replace([X | List], Index, Elem, [X | List2]) :- NIndex is Index - 1, replace(List, NIndex, Elem, List2).
+replace([X | List], Index, Elem, [X | List2]) :- 
+    NIndex is Index - 1, replace(List, NIndex, Elem, List2).
 
 replace(Matrix, I, J, Elem, NewMatrix) :- 
     row(Matrix, I, OldRow), 
@@ -27,43 +30,60 @@ replace(Matrix, I, J, Elem, NewMatrix) :-
 is_dirty((1, _, _, _, _)).
 
 is_env_clean([]).
-is_env_clean([Row | Env]) :- is_row_clean(Row), is_env_clean(Env).
+is_env_clean([Row | Env]) :- 
+    is_row_clean(Row), is_env_clean(Env).
 % is_env_clean([X | Y]) :- not(is_dirty(X, _)), is_env_clean(Y).
 % is_env_clean([(X1, _, _, _, _) | Y]) :- X1 == 0, is_env_clean(Y).
 
 is_row_clean([]).
-is_row_clean([H | T]) :- not(is_dirty(H)), is_row_clean(T).
+is_row_clean([H | T]) :- 
+    not(is_dirty(H)), is_row_clean(T).
 
 count_clean([], 0).
-count_clean([Row | Env], Count) :- not(is_dirty(Row)), count_clean(Env, C), Count is C + 1.
+count_clean([Row | Env], Count) :- 
+    not(is_dirty(Row)), count_clean(Env, C), 
+    Count is C + 1.
 % count_clean([(X1, _, _, _, _)|Y], Z) :- X1 == 0, count_clean(Y, C), Z is C+1.
 % count_clean([(X1, _, _, _, _)|Y], Z) :- X1 == 1, count_clean(Y, C), Z is C.
 
 count_dirty([], 0).
-count_dirty([X | Y], Z) :- is_dirty(X), count_clean(Y, C), Z is C + 1.
+count_dirty([X | Y], Z) :- 
+    is_dirty(X), count_clean(Y, C), 
+    Z is C + 1.
 % count_dirty([(X1, _, _, _, _)|Y], Z) :- X1 == 1, count_dirty(Y, C), Z is C+1.
 % count_dirty([(X1, _, _, _, _)|Y], Z) :- X1 == 0, count_dirty(Y, C), Z is C.
 
 % Tengo dudas aqui, `child` y `robot` no cuentan como "llenar" la casilla?
 count_empty([], 0).
-count_empty([(_, 0, 0, _, _)|Y], Z) :- count_empty(Y, C), Z is C+1.
-count_empty([(_, 1, _, _, _)|Y], C) :- count_empty(Y, C).
-count_empty([(_, _, 1, _, _)|Y], C) :- count_empty(Y, C).
+count_empty([(_, 0, 0, _, _)|Y], Z) :- 
+    count_empty(Y, C), Z is C+1.
+count_empty([(_, 1, _, _, _)|Y], C) :- 
+    count_empty(Y, C).
+count_empty([(_, _, 1, _, _)|Y], C) :- 
+    count_empty(Y, C).
 
 % No entiendo los cortes T_T
 children_captured([]).
-children_captured([(_, _, 1, 1, _)|Y]) :- children_captured(Y).
-children_captured([(_, _, _, 0, _)|Y]) :- children_captured(Y).
+children_captured([(_, _, 1, 1, _)|Y]) :- 
+    children_captured(Y).
+children_captured([(_, _, _, 0, _)|Y]) :- 
+    children_captured(Y).
 
 % There's more than 60% of the env dirty
-poluted(X) :- count_empty(X, C), count_dirty(X, R), R > ((C/100)*60).
+polluted(X) :- 
+    count_empty(X, C), count_dirty(X, R), 
+    R > ((C/100)*60).
 
 final_state([]).
-final_state(X) :- is_env_clean(X), !, children_captured(X).
-final_state(X) :- poluted(X).
+final_state(X) :- 
+    is_env_clean(X), !, children_captured(X).
+final_state(X) :- 
+    polluted(X).
 
 % Es esto inRange?
-validPos(Env, R, C) :- columns(Env, N), rows(Env, M), C > 0, C =< N, R > 0, R =< M.
+validPos(Env, R, C) :- 
+    columns(Env, N), rows(Env, M), 
+    C > 0, C =< N, R > 0, R =< M.
 
 % % Given a row and a column, return index
 % indexFrom(Env, Row, Column, Index) :- 
@@ -126,28 +146,64 @@ child_neighborhood(Env, R, C, [(A, B) | Dirc], Count):-
     Count is Count1 + Count2. 
 
 my_random8(A, B) :- 
-    random_between(1, 8, C), directions8(Dirc), nth1(C, Dirc, (A, B)).
+    random_between(1, 8, C), directions8(Dirc), 
+    nth1(C, Dirc, (A, B)).
 
 my_random4(A, B) :- 
-    random_between(1, 4, C), directions4(Dirc), nth1(C, Dirc, (A, B)).
+    random_between(1, 4, C), directions4(Dirc),
+    nth1(C, Dirc, (A, B)).
 
-give_random_pos([X], [], X).
-eliminar_elemento_random(P1, P2, X) :- length_Lista(P1, L), random_between(1, L, C), eliminar_elemento(P1, P2, C, X).
+get_random_element([X], [], X).
+get_random_element(List, Rest, Element) :- 
+    length(List, L), random_between(1, L, C), 
+    nth1(C, List, Element, Rest).
 
-eliminar_elemento([X|Y], [Y], 1, X).
-eliminar_elemento([X|Y], [X|Z], Count, C) :- Count2 is Count-1, eliminar_elemento(Y, Z, Count2, C).
+mess_direc(Env1, R, C, Env2) :-
+    validPos(Env1, R, C), 
+    index(Env1, R, C, (0, 0, 0, X4, X5)), !,
+    replace(Env1, R, C, (1, 0, 0, X4, X5), Env2).
+mess_direc(Env1, _, _, Env1).
 
-agregar_tupla_a_lista(P1, N, M, C, R, P1) :- not(validPos(N, M, C, R)), !.
-agregar_tupla_a_lista(P1, N, M, C, R, [(C, R)|P1]) :- validPos(N, M, C, R).
+mess_child_count(Env1, R, C, Env2, Count) :- 
+    Count == 0, !, directions8(Direc), 
+    get_random_element(Direc, _, (X, Y)), 
+    R1 is R+X, C1 is C+Y, 
+    mess_direc(Env1, R1, C1, Env2).
+mess_child_count(Env1, R, C, Env2, Count) :- 
+    Count == 1, !, directions8(Direc), 
+    get_random_element(Direc, Rest1, (X1, Y1)), 
+    R1 is R+X1, C1 is C+Y1, 
+    mess_direc(Env1, R1, C1, Env3),
+    get_random_element(Rest1, _, (X2, Y2)), 
+    R2 is R+X2, C2 is C+Y2, 
+    mess_direc(Env3, R2, C2, Env2).
+mess_child_count(Env1, R, C, Env2, _) :- 
+    directions8(Direc), 
+    get_random_element(Direc, Rest1, (X1, Y1)), 
+    R1 is R+X1, C1 is C+Y1, 
+    mess_direc(Env1, R1, C1, Env3),
+    get_random_element(Rest1, Rest2, (X2, Y2)), 
+    R2 is R+X2, C2 is C+Y2, 
+    mess_direc(Env3, R2, C2, Env4),
+    get_random_element(Rest2, Rest3, (X3, Y3)), 
+    R3 is R+X3, C3 is C+Y3, 
+    mess_direc(Env4, R3, C3, Env5),
+    get_random_element(Rest3, Rest4, (X4, Y4)), 
+    R4 is R+X4, C4 is C+Y4, 
+    mess_direc(Env5, R4, C4, Env6),
+    get_random_element(Rest4, Rest5, (X5, Y5)), 
+    R5 is R+X5, C5 is C+Y5, 
+    mess_direc(Env6, R5, C5, Env7),
+    get_random_element(Rest5, _, (X6, Y6)), 
+    R6 is R+X6, C6 is C+Y6, 
+    mess_direc(Env7, R6, C6, Env2).
 
-formar_lista_8v(N, M, C, R, L1) :- C1 is C+1, C2 is C-1, R1 is R+1, R2 is R-1, agregar_tupla_a_lista([], N, M, C, R2, L2), agregar_tupla_a_lista(L2, N, M, C1, R2, L3), agregar_tupla_a_lista(L3, N, M, C1, R, L4), agregar_tupla_a_lista(L4, N, M, C1, R1, L5), agregar_tupla_a_lista(L5, N, M, C, R1, L6), agregar_tupla_a_lista(L6, N, M, C2, R1, L7), agregar_tupla_a_lista(L7, N, M, C2, R, L8), agregar_tupla_a_lista(L8, N, M, C2, R2, L1).
+mess_child(Env1, R, C, Env2) :- 
+    directions8(Direc), 
+    child_neighborhood(Env1, R, C, Direc, Count),
+    mess_child_count(Env1, R, C, Env2, Count).
 
-ensuciar_si_es_posible(P1, N, P1, C, R) :- get_tupla(P1, N, C, R, (X1, X2, X3, _, _)), (X1 == 1; X2 == 1; X3 == 1), !.
-ensuciar_si_es_posible(P1, N, P2, C, R) :- get_tupla(P1, N, C, R, (X1, X2, X3, X4, X5)), X1 == 0, X2 == 0, X3 == 0, replace((1, X2, X3, X4, X5), P1, N, P2, C, R).
-
-ensuciar_segun_ninnos(P1, N, P2, Count, L) :- Count == 0, !, eliminar_elemento_random(L, _, (C1, R1)), ensuciar_si_es_posible(P1, N, P2, C1, R1).
-ensuciar_segun_ninnos(P1, N, P2, Count, L) :- Count == 1, !, eliminar_elemento_random(L, _, (C1, R1)), ensuciar_si_es_posible(P1, N, P2, C1, R1).
-
-ninno_ensuciar(P1, N, M, C, R, P2) :- contar_ninnos_en_vecindad(P1, N, M, C, R, Count), formar_lista_8v(N, M, C, R, L1), ensuciar_segun_ninnos(P1, N, P2, Count, L1).
-
-agente_ninno(P1, N, M, C, R, P2) :- my_random_8(A, B), ninno_ensuciar(P1, N, M, C, R, P3), move_child(P3, N, M, C, R, P2, A, B).
+child(Env1, R, C, Env2) :- 
+    mess_child(Env1, R, C, Env3), 
+    my_random8(A, B), 
+    move_child(Env3, R, C, A, B, Env2).
